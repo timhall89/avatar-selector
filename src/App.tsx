bc440c57eeb7c6c80892b48615eef61e22b2247e
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-20 23:34:20 +0100

    working out how to get the spinning border on an avatar picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import images from './assets/images';
 
 function App() {
   return (
-    <div>Avatar Selector</div>
+    <>
+      <div>Avatar Selector</div>
+      <div>
+        <div className="spin"></div>
+        <img className="avatar-photo list selecting" src={images['avatar1.png']} alt="Avatar1Image" />
+      </div>
+
+    </>
   );
 }
 
