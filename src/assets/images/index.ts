7246fc563cbb371844f68051e05b84d448602df7
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-20 20:11:38 +0100

    adding all images with index file for looking up image name to get import

---
+++ src/assets/images/index.ts
@@ -0,0 +1,15 @@
+import Avatar1Image from './avatar1.png';
+import Avatar2Image from './avatar2.png';
+import Avatar3Image from './avatar3.png';
+import Avatar4Image from './avatar4.png';
+import Avatar5Image from './avatar5.png';
+import Avatar6Image from './avatar6.png';
+
+export default ({
+  'avatar1.png': Avatar1Image,
+  'avatar2.png': Avatar2Image,
+  'avatar3.png': Avatar3Image,
+  'avatar4.png': Avatar4Image,
+  'avatar5.png': Avatar5Image,
+  'avatar6.png': Avatar6Image,
+});
