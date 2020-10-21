7656819ac7a33cd42bfbde9336688fe5f59c9c68
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-21 23:40:34 +0100

    fleshing out the App component sto run the avatar selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
+import AvatarSelector from './components/AvatarSelector';
 import images from './assets/images';
+import Avatar from './types/Avatar';
+
+const avatarsRaw: Avatar[] = [
+  { "src": "avatar1.png", "label": "Avatar 1", "id": 1 },
+  { "src": "avatar2.png", "label": "Avatar 2", "id": 2 },
+  { "src": "avatar3.png", "label": "Avatar 3", "id": 3 },
+  { "src": "avatar4.png", "label": "Avatar 4", "id": 4 },
+  { "src": "avatar5.png", "label": "Avatar 5", "id": 5 },
+  { "src": "avatar6.png", "label": "Avatar 6", "id": 6 }
+]
+
+const avatars: Avatar[] = avatarsRaw.map(avatar => ({ ...avatar, src: images[avatar.src] }))
 
 function App() {
-  return (
-    <>
-      <div>Avatar Selector</div>
-      <div>
-        <div className="spin"></div>
-        <img className="avatar-photo list selecting" src={images['avatar1.png']} alt="Avatar1Image" />
-      </div>
+  const [selected, setSelected] = useState<Avatar>(avatars[0]);
 
-    </>
+  const onAvatarSelected = useCallback(async (avatar: Avatar) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        setSelected(avatar);
+        resolve()
+      }, 1000);
+    })
+  }, [])
+
+  return (
+    <main className="app">
+      <h1>Avatar Selector</h1>
+      <AvatarSelector avatars={avatars} selected={selected} onAvatarSelected={onAvatarSelected} />
+    </main>
   );
 }
 
