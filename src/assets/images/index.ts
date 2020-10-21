30baaa108cbd19a5ac3ef55475365c978791ef2c
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-21 13:38:48 +0100

    add index signatures for images object

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -5,11 +5,13 @@ import Avatar4Image from './avatar4.png';
 import Avatar5Image from './avatar5.png';
 import Avatar6Image from './avatar6.png';
 
-export default ({
+const imageDictionary: { [index: string]: string } = ({
   'avatar1.png': Avatar1Image,
   'avatar2.png': Avatar2Image,
   'avatar3.png': Avatar3Image,
   'avatar4.png': Avatar4Image,
   'avatar5.png': Avatar5Image,
   'avatar6.png': Avatar6Image,
-});
+})
+
+export default imageDictionary;
