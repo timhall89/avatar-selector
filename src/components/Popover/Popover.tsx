0f41373d814ec505c4d41b9b6cd355909cf80cd5
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-22 00:30:23 +0100

    preventing the hide css animation from running on load

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import './Popover.css'
 
@@ -7,14 +7,20 @@ interface PopoverProps {
   children: React.ReactNode;
 }
 
-const Popover = ({ show, children }: PopoverProps) => (
-  <div className={`avatar-selector-popover ${show ? 'show' : ''}`}>
-    <div className="triangle" />
-    <div className="popover-inner">
-      {children}
+const Popover = ({ show, children }: PopoverProps) => {
+  // ref used to stop hide css animation from showing when first loaded.
+  const firstRenderRef = useRef(true);
+  const firstRender = firstRenderRef.current;
+  if (firstRender) firstRenderRef.current = false;
+  return (
+    <div className={`avatar-selector-popover ${firstRender ? '' : (show ? 'show' : 'hide')}`}>
+      <div className="triangle" />
+      <div className="popover-inner">
+        {children}
+      </div>
     </div>
-  </div>
-)
+  )
+};
 
 Popover.propTypes = {
   show: PropTypes.bool.isRequired,
